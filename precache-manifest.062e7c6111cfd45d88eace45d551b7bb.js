b8c68d4fb5b9c575b15e7f95f52aba447140d6e2
= <arthurreis074@gmail.com>
2020-07-04 02:14:34 -0300

    Updates

---
+++ precache-manifest.062e7c6111cfd45d88eace45d551b7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ff5723f55f94ae9958007994e5b7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e6bebb4cbb9cb34481",
+    "url": "/static/js/2.1a169953.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1a169953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5c41de5b37a48b7582",
+    "url": "/static/js/main.56d9f3cb.chunk.js"
+  },
+  {
+    "revision": "eb2c4032df6f98103210",
+    "url": "/static/js/runtime-main.a3618405.js"
+  },
+  {
+    "revision": "f8e3250f21c0a5a23468b2cc934ef7e6",
+    "url": "/static/media/menu-icon.f8e3250f.svg"
+  },
+  {
+    "revision": "c29f7eeedc32458ce18a017a628d1697",
+    "url": "/static/media/user-icon.c29f7eee.svg"
+  }
+]);
