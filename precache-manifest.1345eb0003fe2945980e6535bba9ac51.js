f1b269eaad989cb694422a841544e3ead0db5e8f
= <arthurreis074@gmail.com>
2020-07-04 02:29:15 -0300

    Updates

---
+++ precache-manifest.1345eb0003fe2945980e6535bba9ac51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c2814ea66e36735ccb07689aa529a8",
+    "url": "/Exploud-Web/index.html"
+  },
+  {
+    "revision": "c2ab99c37fd56f499826",
+    "url": "/Exploud-Web/static/js/2.bb9b71a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Exploud-Web/static/js/2.bb9b71a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378cc17421e22a1eda9f",
+    "url": "/Exploud-Web/static/js/main.1d7b0ba5.chunk.js"
+  },
+  {
+    "revision": "93b9e590ea4ebb64beab",
+    "url": "/Exploud-Web/static/js/runtime-main.ea0efa8b.js"
+  },
+  {
+    "revision": "f8e3250f21c0a5a23468b2cc934ef7e6",
+    "url": "/Exploud-Web/static/media/menu-icon.f8e3250f.svg"
+  },
+  {
+    "revision": "c29f7eeedc32458ce18a017a628d1697",
+    "url": "/Exploud-Web/static/media/user-icon.c29f7eee.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.062e7c6111cfd45d88eace45d551b7bb.js"
+  "/Exploud-Web/precache-manifest.1345eb0003fe2945980e6535bba9ac51.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Exploud-Web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
